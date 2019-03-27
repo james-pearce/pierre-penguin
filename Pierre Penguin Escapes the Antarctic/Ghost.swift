49290a18288b506a8f88451527620a886490198f
James E Pearce <pearce.je@gmail.com>
2019-03-27 21:15:28 +1100

    ghost

---
+++ Pierre Penguin Escapes the Antarctic/Ghost.swift
@@ -0,0 +1,9 @@
+//
+//  Ghost.swift
+//  Pierre Penguin Escapes the Antarctic
+//
+//  Created by James Pearce on 27/3/19.
+//  Copyright © 2019 3 Crowns Consulting. All rights reserved.
+//
+
+import Foundation
