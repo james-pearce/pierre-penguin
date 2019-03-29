2a3f349c5d485eeb3d954e981125b94eeadb6c2d
James E Pearce <pearce.je@gmail.com>
2019-03-29 18:31:50 +1100

    coin

---
+++ Coin.swift
@@ -0,0 +1,9 @@
+//
+//  Coin.swift
+//  Pierre Penguin Escapes the Antarctic
+//
+//  Created by James Pearce on 29/3/19.
+//  Copyright © 2019 3 Crowns Consulting. All rights reserved.
+//
+
+import Foundation
