14fe5a4d8ccea78315d10bc09fa7dbbb3e194729
James E Pearce <pearce.je@gmail.com>
2019-03-30 17:34:33 +1100

    flying

---
+++ Pierre Penguin Escapes the Antarctic/EncounterManager.swift
@@ -0,0 +1,9 @@
+//
+//  EncounterManager.swift
+//  Pierre Penguin Escapes the Antarctic
+//
+//  Created by James Pearce on 30/3/19.
+//  Copyright © 2019 3 Crowns Consulting. All rights reserved.
+//
+
+import Foundation
